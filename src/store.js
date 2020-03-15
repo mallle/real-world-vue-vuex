aefd3266c05fb8940f1b9baad958c80141ee7642
Malene <maleneklit@hotmail.com>
2020-03-15 11:46:59 +0100

    Add limited API calls and pagigation to list of events

--- src/store.js
+++ src/store.js
@@ -17,6 +17,8 @@ export default new Vuex.Store({
       'community'
     ],
     events: [],
+    event: {},
+    eventsTotal: 0,
     todos: [
       { id: 1, text: '...', done: true },
       { id: 2, text: '...', done: false },
@@ -28,6 +30,15 @@ export default new Vuex.Store({
     //Event is the payload coming from the component
     ADD_EVENT(state, event) {
       state.events.push(event)
+    },
+    SET_EVENTS(state, events) {
+      state.events = events
+    },
+    SET_EVENT(state, event) {
+      state.event = event
+    },
+    SET_EVENTS_TOTAL(state, eventsTotal) {
+      state.eventsTotal = eventsTotal
     }
   },
   actions: {
@@ -36,6 +47,39 @@ export default new Vuex.Store({
       return EventService.postEvent(event).then(() => {
         commit('ADD_EVENT', event)
       })
+    },
+    fetchEvents({ commit }, { perPage, page }) {
+      EventService.getEvents(perPage, page)
+        .then(response => {
+          console.log('Total: ' + response.headers['x-total-count'])
+          commit(
+            'SET_EVENTS_TOTAL',
+            parseInt(response.headers['x-total-count'])
+          )
+          commit('SET_EVENTS', response.data)
+        })
+        .catch(error => {
+          console.log('There was an error:', error.response)
+        })
+    },
+    fetchEvent({ commit, getters }, id) {
+      //because we already have all events in event,
+      //we can use the getter to return only one event
+      //instead of sending a new API call.
+      //If we don't find the event in the current events
+      //then we do send another request to the API.
+      const event = getters.getEventById(id)
+      if (event) {
+        commit('SET_EVENT', event)
+      } else {
+        EventService.getEvent(id)
+          .then(response => {
+            commit('SET_EVENT', response.data)
+          })
+          .catch(error => {
+            console.log('There was an error:', error.response)
+          })
+      }
     }
   },
   getters: {
@@ -50,6 +94,9 @@ export default new Vuex.Store({
     },
     getTodoById: state => id => {
       return state.todos.find(todo => todo.id === id)
+    },
+    getEventById: state => id => {
+      return state.events.find(event => event.id === id)
     }
   }
 })
--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,31 +1,49 @@
 <template>
   <div>
     <h1>Events Listing</h1>
-    <EventCard v-for="event in events" :key="event.id" :event="event"/>
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <template v-if="page != 1">
+      <router-link
+        :to="{ name: 'event-list', query: { page: page - 1 } }"
+        rel="prev"
+        >Prev Page</router-link
+      >
+      |
+    </template>
+    <router-link
+      :to="{ name: 'event-list', query: { page: page + 1 } }"
+      v-if="hasNextPage"
+      >Next Page</router-link
+    >
   </div>
 </template>
 
 <script>
 import EventCard from '@/components/EventCard.vue'
-import EventService from '@/services/EventService.js'
+import { mapState } from 'vuex'
 
 export default {
   components: {
     EventCard
   },
-  data() {
-    return {
-      events: []
-    }
-  },
   created() {
-    EventService.getEvents()
-      .then(response => {
-        this.events = response.data
-      })
-      .catch(error => {
-        console.log('There was an error:', error.response)
-      })
+    this.perPage = 3 // Setting perPage here and not in data means it won't be reactive.
+    // We don't need it to be reactive, and this way our component has access to it.
+    //call the action from state
+    this.$store.dispatch('fetchEvents', {
+      // perPage: 3,
+      perPage: this.perPage,
+      page: this.page
+    })
+  },
+  computed: {
+    page() {
+      return parseInt(this.$route.query.page) || 1
+    },
+    hasNextPage() {
+      return this.eventsTotal > this.page * this.perPage
+    },
+    ...mapState(['events', 'eventsTotal'])
   }
 }
 </script>
